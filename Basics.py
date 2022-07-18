c1f228c62f785d2f4854d5d414dbb0dfddf412b1
eddyndumia <ndumiaeddy@gmail.com>
2022-07-18 19:59:20 +0300

    Initial commit

---
+++ Basics.py
@@ -0,0 +1,11 @@
+# Basics Python
+
+##
+# Strings
+sentence1 = "the lazy dog jumped over the dog"
+verb = "dog" in sentence1
+print(verb)
+
+sentence2 = "I will get rich within the next 10 years"
+noun = "rich" not in sentence2
+print(noun)
---
+++ Integers.py
@@ -0,0 +1,11 @@
+# Integers
+age = 4
+distance = 10
+print(age * distance)
+
+# Integer Division
+print(4//2)
+
+# Floating Division
+print(10/5)
+
