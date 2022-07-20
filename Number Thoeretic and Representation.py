713dcbdf72ec8867b2b66caa5cf9b21081181152
eddyndumia <ndumiaeddy@gmail.com>
2022-07-20 14:54:55 +0300

    Python Basics revision

---
+++ Number Thoeretic and Representation.py
@@ -0,0 +1,14 @@
+# Number theoretic and representation functions from the math module
+
+import math
+
+x = 2.2
+print(math.ceil(x))  # This function rounds up the float up to the nearest integer
+
+y = -2.2
+z = 5
+print(math.copysign(x, z))  # This function takes two parameters. The first parameter is the number you want to keep and
+# the second is the sign you want to copy
+
+a = -22
+print(math.fabs(a))  # This function prints the absolute number of the parameter value passed
