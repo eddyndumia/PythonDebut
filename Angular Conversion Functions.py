e957c52f40b5a95dd8a164854f7846396c5d49f1
eddyndumia <ndumiaeddy@gmail.com>
2022-07-20 15:52:26 +0300

    Python Basics revision

---
+++ Angular Conversion Functions.py
@@ -0,0 +1,15 @@
+# This sector focuses on the angular section of mathematics and degrees
+# A degree is a unit of measurement in mathematics
+# There are 180 degrees in a straight line
+# There are 360 degrees in a circle
+# A radian is a degree times 180 divided by pi
+
+import math
+
+radians = 1.5
+radians_to_dg = math.degrees(1.5)  # This will convert radians to degrees
+print(radians_to_dg)
+
+dg_to_radians = math.radians(radians_to_dg)  # This will convert degrees to radians
+print(dg_to_radians)
+
