baefc749d9bfda9e2f1fb8fcbb5354f0a6f2e398
eddyndumia <ndumiaeddy@gmail.com>
2022-07-20 14:45:15 +0300

    Python Basics revision

---
+++ Constants.py
@@ -0,0 +1,26 @@
+# The Python math library gives us access to constant mathematical variables as well as mathematical functions such as
+# Euler's Number, Pi, Tau,NAN and Infinity
+
+# Start by importing the math library
+import math
+pi = math.pi
+print(pi)
+
+tau = math.tau
+print(tau)
+
+euler = math.e
+print(euler)
+
+# Abstract constant  -> These include NAN (non-constant) and INF (infinity) which are different from the three above
+non_constant = math.nan
+print(non_constant)
+
+infinity = math.inf
+print(infinity)
+
+
+
+
+
+
---
+++ Python Math Module.py
@@ -0,0 +1,5 @@
+# Python Math module gives us access to some commonly used mathematical functions
+# E.g. Constants, Hyperbolic Functions, Angular Conversion Functions, Number theory and representation etcetra.
+
+
+
