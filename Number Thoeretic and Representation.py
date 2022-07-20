7405c81a938784cb26ddecaab095dbfd272638b5
eddyndumia <ndumiaeddy@gmail.com>
2022-07-20 15:04:44 +0300

    Python Basics revision

--- Number Thoeretic and Representation.py
+++ Number Thoeretic and Representation.py
@@ -4,6 +4,7 @@ import math
 
 x = 2.2
 print(math.ceil(x))  # This function rounds up the float up to the nearest integer
+print(math.floor(x))  # This is the opposite of .ceil() and will truncate the float to the nearest integer down
 
 y = -2.2
 z = 5
@@ -12,3 +13,23 @@ print(math.copysign(x, z))  # This function takes two parameters. The first para
 
 a = -22
 print(math.fabs(a))  # This function prints the absolute number of the parameter value passed
+
+b = 4
+print(math.factorial(b))  # This function will get the factorial of the number passed. For this instance 4 x 3 x 2 x 1
+
+# sqrt -> will get the square root of an integers
+c = 25
+print(math.sqrt(c))
+
+# gcd -> gets the greatest common divisor of two integers
+d = 15
+print(math.gcd(c,d))
+
+
+# fsum -> gets the sum of a collection of integers
+
+collection = [2, 4, 5, 8]
+print(math.fsum(collection))
+
+# lcm -> gets the least common multiple of two integers
+print(math.lcm(d,c))
