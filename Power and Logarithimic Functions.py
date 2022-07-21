7d2e7ac409f219ad56e37ac9f438e596eb44c38c
eddyndumia <ndumiaeddy@gmail.com>
2022-07-21 10:16:38 +0300

    Python Basics revision

---
+++ Power and Logarithimic Functions.py
@@ -0,0 +1,26 @@
+import math
+
+# Logarithmic and Power functions
+
+x = math.exp(2)  # This will return exponential raised to the parameter value
+print(x)
+
+y = math.expm1(3)  # Returns the exponential raised to the parameter value minus 1
+print(y)
+z = math.log(21)  # Gives us the logarithm of the number we pass
+print(z)
+
+a = math.log1p(20)  # log1p() method returns log(1+number)
+print(a)
+
+b = math.log2(100)  # Returns the base2 logarithm of a number
+print(b)
+
+c = math.log10(100)  # Returns the bas10 logarithm of the number we specify
+print(c)
+
+d = math.pow(10, 2)  # Returns the first parameter value raised to the second
+print(d)
+
+e = math.sqrt(100)  # Returns the square root of the number specified
+print(e)
