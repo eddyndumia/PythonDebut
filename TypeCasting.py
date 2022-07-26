dd21d660b9d57605c93b2126a8528d335cfe489d
Eddy Ndumia <ndumiaeddy@gmail.com>
2022-07-26 10:36:22 +0300

    Python Basics revision

---
+++ TypeCasting.py
@@ -0,0 +1,9 @@
+# This is converting variables from one data type to the other
+
+a = "1234567"
+
+print(str(a))      # This was converted to a string
+
+b = "1.2"
+print(float(b))    # Was converted to a float
+
