49eea915f9bc681aad2b0b539eb82fbb37e6b8b9
Eddy Ndumia <ndumiaeddy@gmail.com>
2022-07-26 11:41:06 +0300

    Python Basics revision

---
+++ Recursion.py
@@ -0,0 +1,15 @@
+# Recursion is a way of solving problems by having a function call itself
+# Rules of recursion:
+# 1. Performing the same operations multiple times with different inputs
+# 2. In every step we try smaller inputs to make the issue smaller
+# 3. Base condition is needed to stop recursion otherwise infinite loop will occur
+
+# why recursion?
+# When to use recursion (it's very similar to iteration, but they have different use cases
+# - if you can break down the issues into similar sub-issues
+# - design an algorithm to compute nth
+# - write a code to list n
+# - implement a method to compute all
+
+# The prominent usage of recursion is in trees and graphs
+
---
+++ Vowel Eater.py
@@ -0,0 +1,10 @@
+# Vowel EATERRRRRRR!!
+name = str(input("Enter your name please:"))
+
+name = name.upper()
+length = len(name)
+for i in range(length):
+    for letter in name[i]:
+        if letter not in ('A', 'E', 'I', 'O', 'U'):
+            print(letter)
+            continue
